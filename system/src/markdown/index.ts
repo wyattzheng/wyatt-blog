a26cf40a2fdd5d482abe2872ea4e37a0181e8556
huaiyu zheng <854185073@qq.com>
2021-05-17 20:25:21 +0800

    fix: 移除Markdown在中文换行之间的空格

--- system/src/markdown/index.ts
+++ system/src/markdown/index.ts
@@ -5,6 +5,7 @@ import { RemarkDirectiveImage } from "./remark-directive-image";
 
 import * as RemarkParse from "remark-parse";
 import * as Remark2Rehype from "remark-rehype";
+import * as RemarkJoinCJKLines from "remark-join-cjk-lines";
 import * as Unified from "unified";
 import * as RehypeStringify from "rehype-stringify";
 import * as RemarkDirective from "remark-directive"
@@ -15,6 +16,7 @@ import * as RemarkDirective from "remark-directive"
 export function WCompileMarkdown(markdown_text:string,url_transformer:(img_url:string)=>Promise<string>){
     return Unified()
         .use(RemarkParse)
+        .use(RemarkJoinCJKLines)
         .use(RemarkDirective)
         .use(RemarkDirectiveImage)
         .use(Remark2Rehype)
